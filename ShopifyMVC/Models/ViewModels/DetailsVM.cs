4cb91c23cc5ffdef9fc1c3b9b0d4fb11ef2e8d97
Joshua Oladipo <joshuaoladipo92@gmail.com>
2021-04-19 23:00:07 +0100

    Added SessionExtention Static Class for setting and getting an object to a SessionDictionary Object

--- ShopifyMVC/Models/ViewModels/DetailsVM.cs
+++ ShopifyMVC/Models/ViewModels/DetailsVM.cs
@@ -8,5 +8,6 @@ namespace ShopifyMVC.Models.ViewModels
     public class DetailsVM
     {
         public Product Product { get; set; }
+        public bool ExistInCart { get; set; }
     }
 }
---
+++ ShopifyMVC/SessionExtentions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopifyMVC
+{
+    public static class SessionExtentions
+    {
+        /*
+         * Creating a Set Method that can help to store Object/List of Object to a Session
+         */
+        public static void Set<T>(this ISession session, string key, T value)
+        {
+
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+
+        public static T Get<T>(this ISession session, string key)
+        {
+            //We will first Check the Session Object to be sure that there is an object value in it
+            var value = session.GetString(key);
+
+            if (value == null)
+            {
+                return default;
+            }
+            else
+                
+            return JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+}
